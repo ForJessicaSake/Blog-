52e4eb7f8248edb5b45a4be48a168ac604c2c5a9
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-10-11 19:38:35 +0100

    testing the rest api

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,11 +5,11 @@ const BlogList = () => {
 
     const { id } = useParams();
     const navigate = useNavigate();
-    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id)
+    const { data: blog, isPending, error } = useFetch('https://forjessicasake.github.io/users/db.json/' + id)
 
 
     const handleDelete = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
+        fetch('https://forjessicasake.github.io/users/db.json/' + blog.id, {
             method: "DELETE"
         }).then(() => {
             navigate('/')
@@ -24,9 +24,9 @@ const BlogList = () => {
             {blog && (
 
                 <section className="blog-preview" key={blog.id}>
-                    <h2 className='blog-title'>{blog.title}</h2>
-                    <p className='blog-body'>{blog.body}</p>
-                    <h4 className='blog-author'>{blog.author}</h4>
+                    <h2 className='blog-title'>{blog.username}</h2>
+                    <p className='blog-body'>{blog.email}</p>
+                    <h4 className='blog-author'>{blog.name}</h4>
                     <button className="delete-btn" onClick={handleDelete}>Delete Blog</button>
                 </section>)
 
